123e1c4458be0b1ce21d2ab438992d1159852b61
Rouzrise <rouzrise@gmail.com>
2018-06-02 21:23:33 +0300

    Add Work Experience section

--- dist/js/resumeBuilder.js
+++ dist/js/resumeBuilder.js
@@ -17,17 +17,40 @@ var model = {
         second: '2 skill'
     },
     biopic: 'images/fry.jpg'
-    }
+    },
+
+    //data for jobs section
+    jobs: [{
+        employer: 'ABVGDE Inc',
+        title: 'Junior Graphics Designer',
+        location: 'Moscow, Russia',
+        dates: '2017/08 - in progress',
+        description: 'Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words.'
+    },
+    {
+        employer: 'QUER Inc',
+        title: 'Junior Graphics Designer',
+        location: 'Moscow, Russia',
+        dates: '2015/10 - 2017/05',
+        description: 'Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words. Many interesting words.'
+    },
+]
 };
 
 var controller = {
     init: function() {
         viewBio.init();
+        viewJobs.init();
     },
 
     //gets bio data from model
     getBio: function() {
         return model.bio;
+    },
+
+    //gets jobs data from model
+    getJobs: function() {
+        return model.jobs;
     }
 };
 
@@ -108,6 +131,50 @@ var viewBio = {
             contactGeneric.append(contactGenericSecondSpan);
 
             topContacts.append(contactGeneric);    
+        }
+    }
+};
+
+var viewJobs = {
+    init: function() {
+        var jobs = controller.getJobs();
+
+        var workExperience = document.getElementById('workExperience');
+
+        for (key in jobs) {//loops through jobs object
+        //creates work-entry div
+        this.HTMLworkStart = document.createElement('div');
+        this.HTMLworkStart.className = "work-entry";
+
+        //creates Employer and Job Title element
+        this.HTMLworkEmployerTitle = document.createElement('a');
+        this.HTMLworkEmployerTitle.href = "#"
+        this.HTMLworkEmployerTitle.innerHTML = `${jobs[key].employer} - ${jobs[key].title}`
+
+        this.HTMLworkStart.append(this.HTMLworkEmployerTitle)
+        workExperience.append(this.HTMLworkStart);
+
+        //creates work dates element
+        this.HTMLworkDates = document.createElement('div');
+        this.HTMLworkDates.className = "date-text";
+        this.HTMLworkDates.innerHTML = jobs[key].dates;
+        this.HTMLworkStart.append(this.HTMLworkDates);
+
+        //creates work location element
+        this.HTMLworkLocation = document.createElement('div');
+        this.HTMLworkLocation.className = "location-text";
+        this.HTMLworkLocation.innerHTML = jobs[key].location;
+        this.HTMLworkStart.append(this.HTMLworkLocation);
+
+        //creates line break element
+        this.HTMLemptyTag = document.createElement('br');
+        this.HTMLworkStart.append(this.HTMLemptyTag);
+
+        //creates work description element
+        this.HTMLworkDescription = document.createElement('p');
+        this.HTMLworkDescription.innerHTML = jobs[key]. description;
+        this.HTMLworkStart.append(this.HTMLworkDescription);
+
         }
     }
 };
